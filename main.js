522963b603ee338caedee8cf853b4e395266598f
joshuaarnaez22 <joshuaarnaez22@gmail.com>
2024-10-12 01:21:32 +0800

    ...

--- main.js
+++ main.js
@@ -158,7 +158,9 @@ const loadCanvasJson = async () => {
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.get("/design", (req, res) => {
   loadCanvasJson()
     .then(() => {
